32a4189c2402fd30e2d5000c44796a5b4a5ce819
calvintay <calvin.tws@gmail.com>
2022-03-26 17:07:00 +0800

    V1

--- order_book.py
+++ order_book.py
@@ -23,22 +23,15 @@ def process_order(order):
     session.add(new_order)
     session.commit()
 
-    session.query(Order).filter(Order.filled == None, Order.buy_currency == new_order.sell_currency,
+
+    #search for a matching order
+    matched_orders = session.query(Order).filter(Order.filled == None, Order.buy_currency == new_order.sell_currency,
                                 Order.sell_currency == new_order.buy_currency,
                                 Order.sell_amount / Order.buy_amount <= new_order.buy_amount / new_order.buy_amount)
     #How to do  The buy / sell amounts need not match exactly and Each order should match at most one other
 
-    Order.tx_id = Column(String(256))
-    Order.signature = Column(String(256))
-    Order.timestamp = Column(DateTime, default=datetime.now())  # When was the order inserted into the database
-    Order.counterparty_id = Column(Integer, ForeignKey(
-        'orders.id'))  # If the order was filled (or partially filled) the order_id of the order that acted as the counterparty
-    Order.counterparty = relationship("Order",
-                                foreign_keys='Order.counterparty_id')  # The Order object referenced by counterparty_id
-    Order.filled = Column(DateTime)  # When was the order filled
-    Order.creator_id = Column(Integer, ForeignKey(
-        'orders.id'))  # For derived orders: the order ID of the order that this order was derived from
-    Order.child = relationship("Order", foreign_keys='Order.creator_id', backref=backref('creator', remote_side=[
-        id]))  # set "child" and "creator" to be the derived Order and creating Order objects associated.
+    session.query.order_by(session.total.desc()).all()
+    print(matched_orders)
+
 
     pass
